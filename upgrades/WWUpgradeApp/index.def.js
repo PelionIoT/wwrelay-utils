d09f8045495769e08793a38bce4338f9400f6763
Walt Giroir <walt@giroir.com>
2014-12-18 09:14:43 -0600

    First commit of deviceJS Upgrade App

---
+++ upgrades/WWUpgradeApp/index.def.js
@@ -0,0 +1,13 @@
+module.exports =
+//========Mofify this section========
+{
+    "name": "WWRelayUpgrade",
+    "type": "app",
+    "description" : "WigWag Relay Upgrade Application",
+  	"icon": {
+  		"font": "WigWagIcons",
+  		"character": "j"
+  	},
+    "implements": [ ],
+    "needsServices" : [ "" ]
+};
---
+++ upgrades/WWUpgradeApp/index.js
@@ -0,0 +1,42 @@
+var tools = require('Class/coreTools.js');
+var log = tools.log;
+
+var util = require('util');
+var path = require('path');
+var App = require('Class/App');
+var express = require('express');
+var morgan = require('morgan');
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
+var dev$Promise = require('Class/DevPromise');
+var upgradeUtils = require('./upgradeUtils.js');
+
+
+var WWUpgradeApp = App.create('WWUpgradeApp', function() {
+    var self = this;
+    var app = null;
+
+    this.start = function () {
+        log.debug("-----> wwupgradeapp start("+this.id()+")");
+        return new dev$promise().when(function(p) {
+
+            //  Path to local versions file
+            var versionFile = '/etc/wigwag/versions.json'
+
+            var versCallback = function(a, b) {
+                 console.log("Testing");
+	    };
+
+           upgradeUtils.init(versionFile, versCallback);
+
+        });
+    };
+
+    this.stop = function() {
+        return new dev$Promise().when(function(p) {
+            p.resolve();
+        });
+    };
+});
+
+module.exports = WWUpgradeApp;

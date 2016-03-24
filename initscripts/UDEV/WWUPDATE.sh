7a66b104a2814e3d0ddccb89e367b5997f2d231e
x85446 <trav@izuma.net>
2016-03-24 14:55:13 -0500

    added ability to simply have a tar file wwupdate.tar.gz on the root of a usb key.  The usb UDEV program now untars to /tmp/ then cds to /tmp/ and runs wwupdate.sh

--- initscripts/UDEV/WWUPDATE.sh
+++ initscripts/UDEV/WWUPDATE.sh
@@ -3,27 +3,46 @@
 mountdir="/run/media/WWUPDATE"
 disk="/dev/WWUPDATEp1"
 initscript="wwupdate.sh"
+initball="wwupdate.tar.gz"
+initzip="wwupdate.zip"
 logfile="/wigwag/log/update.log"
 
 
+function logit() {
+	if [[ $1 != "" ]]; then
+		echo $(date)": $1 ">> $logfile
+	fi
+}
+
 #mounts the usb key
 function mounter() {
-	echo "mounting media $disk ">> $logfile
+	logit "mounting media $disk "
 	mkdir -p $mountdir
 	mount -o rw $disk $mountdir
 }
 
 #unoumnts the usb key
 function umounter() {
-	echo "unmounting media $disk ">> $logfile
-	umount $mountdir 
-	umount $disk 
+	logit "unmounting the directory $mountdir"
+	cd /
+	out=$(umount $mountdir)
+	logit "$out"
+	logit "unmounting the directory $disk"
+	out=$(umount $disk)
+	logit "$out"
 }
 
 #runs an init.sh script from the usb key
 function runinit() {
+	logit "inspecting the USB Key for $initscript or $initball"
 cd $mountdir
 if [[ -e $initscript ]]; then
+	logit "runing the $initscript"
+	. $initscript
+elif [[ -e $initball ]]; then
+	logit "untaring $initball to /tmp/"
+	tar -xzf $initball -C /tmp/
+	cd /tmp/
 	. $initscript
 fi
 }
@@ -53,5 +72,5 @@ updater
 popd
 umounter
 else
-	unmounter
+	umounter
 fi

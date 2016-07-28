661b37f6eb8efcff5b3dbf969591c4367a4f53ea
Walt Giroir <walt@giroir.com>
2016-07-28 18:41:52 -0500

    Added updater code

--- WWSupportTunnel/initialize.sh
+++ WWSupportTunnel/initialize.sh
@@ -1,10 +1,20 @@
 #! /bin/bash
 
+if [ ! -e /updater/relay-updater/downloads ]; then
+   mkdir -p /updater/relay-updater/downloads
+fi
+
+if [ ! -e /updater/relay-updater/installs ]; then
+   mkdir -p /updater/relay-updater/downloads
+fi
+
 KHTun=`cat /home/root/.ssh/known_hosts | grep tunnel.wigwag.com`;
 if [ "$KHTun" = "" ]
         then
         cat /wigwag/support/known_hosts >> /home/root/.ssh/known_hosts;
 fi
+
+/wigwag/support/checkForUpdates.sh &
 chmod 600 /wigwag/support/relay_support_key
 chown -R support:support /home/support/.ssh
 node /wigwag/support/index.js

22a41727562ec825c58f00f636239280dd9e5e73
x85446 <trav@izuma.net>
2017-03-06 14:54:48 -0600

    adding minor changes to stopwatch

--- dev-tools/bin/stopwatchdog.sh
+++ dev-tools/bin/stopwatchdog.sh
@@ -3,3 +3,4 @@ KEEPALIVE="/var/deviceOSkeepalive"
 STOP_DEVICEOSWD_CMD="echo -e \"stop\" | socat unix-sendto:$KEEPALIVE STDIO"
 eval "$STOP_DEVICEOSWD_CMD"
 killall deviceOSWD
+rm /var/run/deviceOSWD.pid

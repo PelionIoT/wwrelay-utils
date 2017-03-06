6fb2227cac2693faa5325bf48c9413cfc427d7c9
x85446 <trav@izuma.net>
2017-03-05 21:13:16 -0600

    adding the last

---
+++ dev-tools/bin/stopwatchdog.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+KEEPALIVE="/var/deviceOSkeepalive"
+STOP_DEVICEOSWD_CMD="echo -e \"stop\" | socat unix-sendto:$KEEPALIVE STDIO"
+eval "$STOP_DEVICEOSWD_CMD"
+killall deviceOSWD
--- dev-tools/bin/stopwigwag.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-/dev/init.d/deviceOSWD stop
-/dev/init.d/devicejs stop

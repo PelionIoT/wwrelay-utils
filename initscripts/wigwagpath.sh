f51c633bd65708cd9e1b8bc17f964ff8f118da68
x85446 <trav@izuma.net>
2017-07-07 00:37:09 -0500

    added color prompt to .bashrc

--- initscripts/wigwagpath.sh
+++ initscripts/wigwagpath.sh
@@ -3,11 +3,18 @@ export PS1='\h:\w\$ '
 umask 022
 
 # You may uncomment the following lines if you want `ls' to be colorized:
-export LS_OPTIONS='--color=auto'
-eval `dircolors`
-alias ls='ls $LS_OPTIONS'
-alias ll='ls $LS_OPTIONS -l'
-alias l='ls $LS_OPTIONS -lA'
+color_prompt=yes
+PS1='${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\
+\[\033[00m\]\$ '
+# enable color support of ls and also add handy aliases
+if [ -x /usr/bin/dircolors ]; then
+	test -r ~/.dircolors && eval "$(dircolors -b ~/.dircolors)" || eval "$(dircolors -b)"
+	alias ls='ls --color=auto'
+	alias grep='grep --color=auto'
+	alias fgrep='fgrep --color=auto'
+	alias egrep='egrep --color=auto'
+fi
+
 
 export PATH="/wigwag/system/bin:$PATH"
 export LD_LIBRARY_PATH="/wigwag/system/lib:$LD_LIBRARY_PATH"

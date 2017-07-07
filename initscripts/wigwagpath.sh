0955848af0ac14d874f041ac48d334dc668652b5
x85446 <trav@izuma.net>
2017-07-06 21:05:07 -0500

    fixed two typo errors blocking the tunnel program

--- initscripts/wigwagpath.sh
+++ initscripts/wigwagpath.sh
@@ -1,13 +1,13 @@
-xport HAVEGLOBAL="yes"
+export HAVEGLOBAL="yes"
 export PS1='\h:\w\$ '
 umask 022
 
 # You may uncomment the following lines if you want `ls' to be colorized:
- export LS_OPTIONS='--color=auto'
- eval `dircolors`
- alias ls='ls $LS_OPTIONS'
- alias ll='ls $LS_OPTIONS -l'
- alias l='ls $LS_OPTIONS -lA'
+export LS_OPTIONS='--color=auto'
+eval `dircolors`
+alias ls='ls $LS_OPTIONS'
+alias ll='ls $LS_OPTIONS -l'
+alias l='ls $LS_OPTIONS -lA'
 
 export PATH="/wigwag/system/bin:$PATH"
 export LD_LIBRARY_PATH="/wigwag/system/lib:$LD_LIBRARY_PATH"

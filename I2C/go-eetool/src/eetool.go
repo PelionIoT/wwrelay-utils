2d7bf0e0c63523f406ed54491eee3607713ae5ef
Yash Goyal <ygoyal@wigwgag.com>
2017-09-14 01:28:32 -0500

    Added properties to be compatible with existing config options and also formatted all data to string

--- I2C/go-eetool/src/eetool.go
+++ I2C/go-eetool/src/eetool.go
@@ -4,6 +4,7 @@ import "log"
 import "fmt"
 import I2C "golang.org/x/exp/io/i2c"
 import "regexp"
+import "encoding/hex"
 
 func check(err error) {
     if err != nil { 
@@ -11,42 +12,44 @@ func check(err error) {
     }
 }
 
-type eelayout struct {
+type eeprom_anatomy struct {
     name string
     pageaddr int
     memaddr byte
     length int
-    format string
-    regex string
 } 
 
 type eeData struct {
     name string
-    format string
-    data []byte
+    data string
 }
 
-var metadata = []eelayout {
-    eelayout {"BRAND",            0x50, 0, 2, "string", ""}, 
-    eelayout {"DEVICE",           0x50, 2, 2, "string", ""},
-    eelayout {"UUID",             0x50, 4, 6, "string", ""}, 
-    eelayout {"hardwareVersion",  0x50, 10, 5, "string", ""}, 
-    eelayout {"firmwareVersion",  0x50, 15, 5, "string", ""}, 
-    eelayout {"radioConfig",      0x50, 20, 2, "string", ""}, 
-    eelayout {"year",             0x50, 22, 1, "string", ""}, 
-    eelayout {"month",            0x50, 23, 1, "string", ""}, 
-    eelayout {"batch",            0x50, 24, 1, "string", ""}, 
-    eelayout {"ethernetMAC",      0x50, 25, 6, "byte", ""}, 
-    eelayout {"sixBMAC",          0x50, 31, 8, "byte", ""}, 
-    eelayout {"relaySecret",      0x50, 39, 32, "string", ""}, 
-    eelayout {"pairingCode",      0x50, 71, 25, "string", ""}, 
-    eelayout {"ledConfig",        0x50, 96, 2, "string", ""}, 
-    eelayout {"cloudURL",         0x51, 0, 100, "string", "[^a-zA-Z0-9.:/-]"}, 
-    eelayout {"devicejsCloudURL", 0x52, 0, 100, "string", "[^a-zA-Z0-9.:/-]"}, 
-    eelayout {"devicedbCloudURL", 0x53, 0, 100, "string", "[^a-zA-Z0-9.:/-]"},
+var metadata = []eeprom_anatomy {
+    eeprom_anatomy {"ARCH_BRAND",             0x50, 0, 2   }, 
+    eeprom_anatomy {"ARCH_DEVICE",            0x50, 2, 2   },
+    eeprom_anatomy {"ARCH_UUID",              0x50, 4, 6   }, 
+    eeprom_anatomy {"ARCH_RELAYID",           0x50, 0, 10  }, 
+    eeprom_anatomy {"ARCH_HARDWARE_VERSION",  0x50, 10, 5  }, 
+    eeprom_anatomy {"ARCH_WW_PLATFORM",       0x50, 10, 5  }, 
+    eeprom_anatomy {"ARCH_FIRMWARE_VERSION",  0x50, 15, 5  }, 
+    eeprom_anatomy {"ARCH_RADIO_CONFIG",      0x50, 20, 2  }, 
+    eeprom_anatomy {"ARCH_YEAR",              0x50, 22, 1  }, 
+    eeprom_anatomy {"ARCH_MONTH",             0x50, 23, 1  }, 
+    eeprom_anatomy {"ARCH_BATCH",             0x50, 24, 1  }, 
+    eeprom_anatomy {"ARCH_ETHERNET_MAC",      0x50, 25, 6  },
+    eeprom_anatomy {"ARCH_SIXLBR_MAC",        0x50, 31, 8  },
+    eeprom_anatomy {"ARCH_RELAY_SECRET",      0x50, 39, 32 }, 
+    eeprom_anatomy {"ARCH_PAIRING_CODE",      0x50, 71, 25 }, 
+    eeprom_anatomy {"ARCH_LED_CONFIG",        0x50, 96, 2  }, 
+    eeprom_anatomy {"ARCH_LED_COLOR_PROFILE", 0x50, 96, 2  }, 
+    eeprom_anatomy {"ARCH_CLOUD_URL",         0x51, 0, 250 }, 
+    eeprom_anatomy {"ARCH_CLOUD_DEVJS_URL",   0x52, 0, 250 }, 
+    eeprom_anatomy {"ARCH_CLOUD_DDB_URL",     0x53, 0, 250 },
 }
 
-func get_eeprom(prop eelayout) eeData {
+var regex = "[^a-zA-Z0-9.:/-]"
+
+func get_eeprom(prop eeprom_anatomy) eeData {
 
     bus, err := I2C.Open(&I2C.Devfs{Dev: "/dev/i2c-1"}, prop.pageaddr)          
     if err != nil {                                                             
@@ -60,21 +63,30 @@ func get_eeprom(prop eelayout) eeData {
         check(e)                 
     }
 
-    if len(prop.regex) > 0 {
-        r, _ := regexp.Compile(prop.regex)
-        dataStr := r.ReplaceAllString(string(data), "")
-        data = []byte(dataStr)
+    dataStr := string(data)
+
+    r, _ := regexp.Compile(regex)
+    dataStr = r.ReplaceAllString(string(data), "")
+
+    if prop.name == "ARCH_WW_PLATFORM" {
+        dataStr = "wwrelay_v" + dataStr
     }
 
-    // if prop.format == "string" {
-    //     fmt.Printf("%s --> %s\n", prop.name, data)
-    // } else if prop.format == "byte" {
-    //     fmt.Printf("%s --> %# x\n", prop.name, data)            
-    // }
+    if prop.name == "ARCH_ETHERNET_MAC" || prop.name == "ARCH_SIXLBR_MAC" {
+        dataStr = hex.EncodeToString(data)
+    }
+
+    if prop.name == "ARCH_LED_COLOR_PROFILE" {
+        if dataStr == "02" {
+            dataStr = "RBG"
+        } else {
+            dataStr = "RGB"
+        }
+    }
 
     bus.Close()
 
-    return eeData{prop.name, prop.format, data}
+    return eeData{prop.name, dataStr}
 }
              
 func main() {
@@ -83,10 +95,6 @@ func main() {
     for i := 0; i < len(metadata); i++ {  
         eepromData[i] = get_eeprom(metadata[i])  
 
-        if eepromData[i].format == "string" {
-            fmt.Printf("%s --> %s\n", eepromData[i].name, eepromData[i].data)
-        } else if eepromData[i].format == "byte" {
-            fmt.Printf("%s --> %# x\n", eepromData[i].name, eepromData[i].data)            
-        }
+        fmt.Printf("%s --> %s\n", eepromData[i].name, eepromData[i].data)
     }
 }  

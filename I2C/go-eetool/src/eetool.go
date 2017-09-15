9c1f32a85f47fcc2eaab205be57d1424f9ef79dc
Ed Hemphill <edhemphill@gmail.com>
2017-09-15 09:02:31 -0500

    Update eetool.go

--- I2C/go-eetool/src/eetool.go
+++ I2C/go-eetool/src/eetool.go
@@ -36,37 +36,38 @@ type certs_anatomy struct {
     fname2 string
 }
 
+// NOTE: "ARCH_" is added in automatically by maestro
 var sslCerts = []certs_anatomy {
-    certs_anatomy { "ARCH_CLIENT_KEY_PEM", "/mnt/.boot/.ssl/client.key.pem", ""},
-    certs_anatomy { "ARCH_CLIENT_CERT_PEM", "/mnt/.boot/.ssl/client.cert.pem", ""},
-    certs_anatomy { "ARCH_SERVER_KEY_PEM", "/mnt/.boot/.ssl/server.key.pem", ""},
-    certs_anatomy { "ARCH_SERVER_CERT_PEM", "/mnt/.boot/.ssl/server.cert.pem", ""},
-    certs_anatomy { "ARCH_CA_CERT_PEM", "/mnt/.boot/.ssl/ca.cert.pem", ""},
-    certs_anatomy { "ARCH_INTERMEDIATE_CERT_PEM", "/mnt/.boot/.ssl/intermediate.cert.pem", ""},
-    certs_anatomy { "ARCH_CA_CHAIN_CERT_PEM", "/mnt/.boot/.ssl/ca.cert.pem", "/mnt/.boot/.ssl/intermediate.cert.pem"},
+    certs_anatomy { "CLIENT_KEY_PEM", "/mnt/.boot/.ssl/client.key.pem", ""},
+    certs_anatomy { "CLIENT_CERT_PEM", "/mnt/.boot/.ssl/client.cert.pem", ""},
+    certs_anatomy { "SERVER_KEY_PEM", "/mnt/.boot/.ssl/server.key.pem", ""},
+    certs_anatomy { "SERVER_CERT_PEM", "/mnt/.boot/.ssl/server.cert.pem", ""},
+    certs_anatomy { "CA_CERT_PEM", "/mnt/.boot/.ssl/ca.cert.pem", ""},
+    certs_anatomy { "INTERMEDIATE_CERT_PEM", "/mnt/.boot/.ssl/intermediate.cert.pem", ""},
+    certs_anatomy { "CA_CHAIN_CERT_PEM", "/mnt/.boot/.ssl/ca.cert.pem", "/mnt/.boot/.ssl/intermediate.cert.pem"},
 }
 
 var metadata = []eeprom_anatomy {
-    eeprom_anatomy {"ARCH_BRAND",             0x50, 0, 2   }, 
-    eeprom_anatomy {"ARCH_DEVICE",            0x50, 2, 2   },
-    eeprom_anatomy {"ARCH_UUID",              0x50, 4, 6   }, 
-    eeprom_anatomy {"ARCH_RELAYID",           0x50, 0, 10  }, 
-    eeprom_anatomy {"ARCH_HARDWARE_VERSION",  0x50, 10, 5  }, 
-    eeprom_anatomy {"ARCH_WW_PLATFORM",       0x50, 10, 5  }, 
-    eeprom_anatomy {"ARCH_FIRMWARE_VERSION",  0x50, 15, 5  }, 
-    eeprom_anatomy {"ARCH_RADIO_CONFIG",      0x50, 20, 2  }, 
-    eeprom_anatomy {"ARCH_YEAR",              0x50, 22, 1  }, 
-    eeprom_anatomy {"ARCH_MONTH",             0x50, 23, 1  }, 
-    eeprom_anatomy {"ARCH_BATCH",             0x50, 24, 1  }, 
-    eeprom_anatomy {"ARCH_ETHERNET_MAC",      0x50, 25, 6  },
-    eeprom_anatomy {"ARCH_SIXLBR_MAC",        0x50, 31, 8  },
-    eeprom_anatomy {"ARCH_RELAY_SECRET",      0x50, 39, 32 }, 
-    eeprom_anatomy {"ARCH_PAIRING_CODE",      0x50, 71, 25 }, 
-    eeprom_anatomy {"ARCH_LED_CONFIG",        0x50, 96, 2  }, 
-    eeprom_anatomy {"ARCH_LED_COLOR_PROFILE", 0x50, 96, 2  }, 
-    eeprom_anatomy {"ARCH_CLOUD_URL",         0x51, 0, 250 }, 
-    eeprom_anatomy {"ARCH_CLOUD_DEVJS_URL",   0x52, 0, 250 }, 
-    eeprom_anatomy {"ARCH_CLOUD_DDB_URL",     0x53, 0, 250 },
+    eeprom_anatomy {"BRAND",             0x50, 0, 2   }, 
+    eeprom_anatomy {"DEVICE",            0x50, 2, 2   },
+    eeprom_anatomy {"UUID",              0x50, 4, 6   }, 
+    eeprom_anatomy {"RELAYID",           0x50, 0, 10  }, 
+    eeprom_anatomy {"HARDWARE_VERSION",  0x50, 10, 5  }, 
+    eeprom_anatomy {"WW_PLATFORM",       0x50, 10, 5  }, 
+    eeprom_anatomy {"FIRMWARE_VERSION",  0x50, 15, 5  }, 
+    eeprom_anatomy {"RADIO_CONFIG",      0x50, 20, 2  }, 
+    eeprom_anatomy {"YEAR",              0x50, 22, 1  }, 
+    eeprom_anatomy {"MONTH",             0x50, 23, 1  }, 
+    eeprom_anatomy {"BATCH",             0x50, 24, 1  }, 
+    eeprom_anatomy {"ETHERNET_MAC",      0x50, 25, 6  },
+    eeprom_anatomy {"_IXLBR_MAC",        0x50, 31, 8  },
+    eeprom_anatomy {"RELAY_SECRET",      0x50, 39, 32 }, 
+    eeprom_anatomy {"PAIRING_CODE",      0x50, 71, 25 }, 
+    eeprom_anatomy {"LED_CONFIG",        0x50, 96, 2  }, 
+    eeprom_anatomy {"LED_COLOR_PROFILE", 0x50, 96, 2  }, 
+    eeprom_anatomy {"CLOUD_URL",         0x51, 0, 250 }, 
+    eeprom_anatomy {"CLOUD_DEVJS_URL",   0x52, 0, 250 }, 
+    eeprom_anatomy {"CLOUD_DDB_URL",     0x53, 0, 250 },
 }
 
 var regex = "[^a-zA-Z0-9.:/-]"
@@ -90,11 +91,11 @@ func get_eeprom(prop eeprom_anatomy) eeData {
     r, _ := regexp.Compile(regex)
     dataStr = r.ReplaceAllString(string(data), "")
 
-    if prop.name == "ARCH_WW_PLATFORM" {
+    if prop.name == "WW_PLATFORM" {
         dataStr = "wwrelay_v" + dataStr
     }
 
-    if prop.name == "ARCH_ETHERNET_MAC" || prop.name == "ARCH_SIXLBR_MAC" {
+    if prop.name == "ETHERNET_MAC" || prop.name == "SIXLBR_MAC" {
         dataStr = hex.EncodeToString(data)
     }
 

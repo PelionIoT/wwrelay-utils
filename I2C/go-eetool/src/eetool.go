f89711915b12fad8f3440ef2ca888c6355c5e763
Yash Goyal <ygoyal@wigwgag.com>
2017-09-21 15:07:52 -0500

    Updated eetool to return err and not throw panic

--- I2C/go-eetool/src/eetool.go
+++ I2C/go-eetool/src/eetool.go
@@ -36,54 +36,53 @@ type certs_anatomy struct {
     fname2 string
 }
 
-// NOTE: "ARCH_" is added in automatically by maestro
 var sslCerts = []certs_anatomy {
-    certs_anatomy { "CLIENT_KEY_PEM", "/mnt/.boot/.ssl/client.key.pem", ""},
-    certs_anatomy { "CLIENT_CERT_PEM", "/mnt/.boot/.ssl/client.cert.pem", ""},
-    certs_anatomy { "SERVER_KEY_PEM", "/mnt/.boot/.ssl/server.key.pem", ""},
-    certs_anatomy { "SERVER_CERT_PEM", "/mnt/.boot/.ssl/server.cert.pem", ""},
-    certs_anatomy { "CA_CERT_PEM", "/mnt/.boot/.ssl/ca.cert.pem", ""},
-    certs_anatomy { "INTERMEDIATE_CERT_PEM", "/mnt/.boot/.ssl/intermediate.cert.pem", ""},
-    certs_anatomy { "CA_CHAIN_CERT_PEM", "/mnt/.boot/.ssl/ca.cert.pem", "/mnt/.boot/.ssl/intermediate.cert.pem"},
+    certs_anatomy { "ARCH_CLIENT_KEY_PEM", "/mnt/.boot/.ssl/client.key.pem", ""},
+    certs_anatomy { "ARCH_CLIENT_CERT_PEM", "/mnt/.boot/.ssl/client.cert.pem", ""},
+    certs_anatomy { "ARCH_SERVER_KEY_PEM", "/mnt/.boot/.ssl/server.key.pem", ""},
+    certs_anatomy { "ARCH_SERVER_CERT_PEM", "/mnt/.boot/.ssl/server.cert.pem", ""},
+    certs_anatomy { "ARCH_CA_CERT_PEM", "/mnt/.boot/.ssl/ca.cert.pem", ""},
+    certs_anatomy { "ARCH_INTERMEDIATE_CERT_PEM", "/mnt/.boot/.ssl/intermediate.cert.pem", ""},
+    certs_anatomy { "ARCH_CA_CHAIN_CERT_PEM", "/mnt/.boot/.ssl/ca.cert.pem", "/mnt/.boot/.ssl/intermediate.cert.pem"},
 }
 
 var metadata = []eeprom_anatomy {
-    eeprom_anatomy {"BRAND",             0x50, 0, 2   }, 
-    eeprom_anatomy {"DEVICE",            0x50, 2, 2   },
-    eeprom_anatomy {"UUID",              0x50, 4, 6   }, 
-    eeprom_anatomy {"RELAYID",           0x50, 0, 10  }, 
-    eeprom_anatomy {"HARDWARE_VERSION",  0x50, 10, 5  }, 
-    eeprom_anatomy {"WW_PLATFORM",       0x50, 10, 5  }, 
-    eeprom_anatomy {"FIRMWARE_VERSION",  0x50, 15, 5  }, 
-    eeprom_anatomy {"RADIO_CONFIG",      0x50, 20, 2  }, 
-    eeprom_anatomy {"YEAR",              0x50, 22, 1  }, 
-    eeprom_anatomy {"MONTH",             0x50, 23, 1  }, 
-    eeprom_anatomy {"BATCH",             0x50, 24, 1  }, 
-    eeprom_anatomy {"ETHERNET_MAC",      0x50, 25, 6  },
-    eeprom_anatomy {"_IXLBR_MAC",        0x50, 31, 8  },
-    eeprom_anatomy {"RELAY_SECRET",      0x50, 39, 32 }, 
-    eeprom_anatomy {"PAIRING_CODE",      0x50, 71, 25 }, 
-    eeprom_anatomy {"LED_CONFIG",        0x50, 96, 2  }, 
-    eeprom_anatomy {"LED_COLOR_PROFILE", 0x50, 96, 2  }, 
-    eeprom_anatomy {"CLOUD_URL",         0x51, 0, 250 }, 
-    eeprom_anatomy {"CLOUD_DEVJS_URL",   0x52, 0, 250 }, 
-    eeprom_anatomy {"CLOUD_DDB_URL",     0x53, 0, 250 },
+    eeprom_anatomy {"ARCH_BRAND",             0x50, 0, 2   },
+    eeprom_anatomy {"ARCH_DEVICE",            0x50, 2, 2   },
+    eeprom_anatomy {"ARCH_UUID",              0x50, 4, 6   },
+    eeprom_anatomy {"ARCH_RELAYID",           0x50, 0, 10  },
+    eeprom_anatomy {"ARCH_HARDWARE_VERSION",  0x50, 10, 5  },
+    eeprom_anatomy {"ARCH_WW_PLATFORM",       0x50, 10, 5  },
+    eeprom_anatomy {"ARCH_FIRMWARE_VERSION",  0x50, 15, 5  },
+    eeprom_anatomy {"ARCH_RADIO_CONFIG",      0x50, 20, 2  },
+    eeprom_anatomy {"ARCH_YEAR",              0x50, 22, 1  },
+    eeprom_anatomy {"ARCH_MONTH",             0x50, 23, 1  },
+    eeprom_anatomy {"ARCH_BATCH",             0x50, 24, 1  },
+    eeprom_anatomy {"ARCH_ETHERNET_MAC",      0x50, 25, 6  },
+    eeprom_anatomy {"ARCH_SIXLBR_MAC",        0x50, 31, 8  },
+    eeprom_anatomy {"ARCH_RELAY_SECRET",      0x50, 39, 32 },
+    eeprom_anatomy {"ARCH_PAIRING_CODE",      0x50, 71, 25 },
+    eeprom_anatomy {"ARCH_LED_CONFIG",        0x50, 96, 2  },
+    eeprom_anatomy {"ARCH_LED_COLOR_PROFILE", 0x50, 96, 2  },
+    eeprom_anatomy {"ARCH_CLOUD_URL",         0x51, 0, 250 },
+    eeprom_anatomy {"ARCH_CLOUD_DEVJS_URL",   0x52, 0, 250 },
+    eeprom_anatomy {"ARCH_CLOUD_DDB_URL",     0x53, 0, 250 },
 }
 
 var regex = "[^a-zA-Z0-9.:/-]"
 
-func get_eeprom(prop eeprom_anatomy) eeData {
+func get_eeprom(prop eeprom_anatomy) (eeData, error) {
 
-    bus, err := I2C.Open(&I2C.Devfs{Dev: "/dev/i2c-1"}, prop.pageaddr)          
-    if err != nil {                                                             
-        check(err)                                               
+    bus, err := I2C.Open(&I2C.Devfs{Dev: "/dev/i2c-1"}, prop.pageaddr)
+    if err != nil {
+        return eeData{"", ""}, err
     }
 
     data := make([]byte, prop.length)
 
-    e := bus.ReadReg(prop.memaddr, data)
-    if e != nil {                    
-        check(e)                 
+    err = bus.ReadReg(prop.memaddr, data)
+    if err != nil {
+        return eeData{"", ""}, err
     }
 
     dataStr := string(data)
@@ -91,11 +90,11 @@ func get_eeprom(prop eeprom_anatomy) eeData {
     r, _ := regexp.Compile(regex)
     dataStr = r.ReplaceAllString(string(data), "")
 
-    if prop.name == "WW_PLATFORM" {
+    if prop.name == "ARCH_WW_PLATFORM" {
         dataStr = "wwrelay_v" + dataStr
     }
 
-    if prop.name == "ETHERNET_MAC" || prop.name == "SIXLBR_MAC" {
+    if prop.name == "ARCH_ETHERNET_MAC" || prop.name == "ARCH_SIXLBR_MAC" {
         dataStr = hex.EncodeToString(data)
     }
 
@@ -109,38 +108,45 @@ func get_eeprom(prop eeprom_anatomy) eeData {
 
     bus.Close()
 
-    return eeData{prop.name, dataStr}
+    return eeData{prop.name, dataStr}, err
 }
 
-func read_file(cert certs_anatomy) eeData {
+func read_file(cert certs_anatomy) (eeData, error) {
 
-    data, err1 := ioutil.ReadFile(cert.fname1)
-    check(err1)
+    data, err := ioutil.ReadFile(cert.fname1)
+    if err != nil {
+        return eeData{"", ""}, err
+    }
 
     dataStr := string(data)
 
     if cert.fname2 != "" {
-        data2, err2 := ioutil.ReadFile(cert.fname2)
-        check(err2)
+        data2, err := ioutil.ReadFile(cert.fname2)
+        if err != nil {
+            return eeData{"", ""}, err
+        }
 
         dataStr = dataStr + string(data2)
     }
 
-    return eeData{cert.metavariable, dataStr}
+    return eeData{cert.metavariable, dataStr}, err
 }
 
-func main() {
+func GetPlatformVars() (err error) {
     eepromData := make([]eeData, len(metadata) + len(sslCerts))
 
-    for i := 0; i < len(metadata); i++ {  
-        eepromData[i] = get_eeprom(metadata[i])  
+    for i := 0; i < len(metadata); i++ {
+        eepromData[i], err = get_eeprom(metadata[i])
+        if err != nil {
+            return
+        }
 
         fmt.Printf("%s --> %s\n", eepromData[i].name, eepromData[i].data)
     }
     
     cmd := exec.Command("mount", "/dev/mmcblk0p1", "/mnt/.boot/")
 
-    err := cmd.Run()
+    err = cmd.Run()
     exitCode := 0
     if err != nil {
         if exitError, ok := err.(*exec.ExitError); ok {
@@ -150,17 +156,26 @@ func main() {
         log.Printf("command result, exitCode: %v", exitCode) 
         //if exitCode is 32 that means it is already mounted
         if exitCode == 32 {
+            err = nil
             log.Printf("Already mounted... continuing reading certs")
         } else {
-            panic(err)
+            return
         }
     }
 
 
     for i := len(metadata); i < (len(metadata) + len(sslCerts)); i++ {
-        eepromData[i] = read_file(sslCerts[i - len(metadata)])
+        eepromData[i], err = read_file(sslCerts[i - len(metadata)])
+        if err != nil {
+            return
+        }
 
         fmt.Printf("%s --> %s\n", eepromData[i].name, eepromData[i].data)
     }
+    return
 
+}
+
+func main() {
+    GetPlatformVars()
 }  

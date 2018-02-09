e753fb0619463cd9bcf2158c0471f2dae5c81ff4
x85446 <trav@izuma.net>
2018-02-08 21:02:32 -0600

    newest dogprogrammer with baud rate

--- DOGcontrol/dogProgrammer.sh
+++ DOGcontrol/dogProgrammer.sh
@@ -1,24 +1,46 @@
 #!/bin/bash
 baud=9600
-echo -en "ATTINY OFF: "
-pinctrl wdog_reset 0;
-cat /sys/class/gpio/gpio99/value
-sleep 1;
-pinctrl wdog_reset 1;
-echo -en "ATTINY ON: "
-cat /sys/class/gpio/gpio99/value
-if [[ $1 = "" ]]; then
-	hexfile=/mnt/.boot/AT841WDOG.hex
-else
-	hexfile=$1
-fi
-echo "calling tiny safe boot: tsb -s /dev/ttyS3 -b $baud flash $hexfile"
-tsb -s /dev/ttyS3 -b $baud flash $hexfile
-if [[ $? -eq 0  ]]; then
-	if [[ $1 = "minicom" || $2 = "minicom" ]]; then
-		minicom
+
+
+rebootTiny(){
+	echo -en "ATTINY OFF: "
+	pinctrl wdog_reset 0;
+	cat /sys/class/gpio/gpio99/value
+	sleep 1;
+	pinctrl wdog_reset 1;
+	echo -en "ATTINY ON: "
+	cat /sys/class/gpio/gpio99/value
+	
+}
+
+programTiny(){
+	if [[ $1 = "" ]]; then
+		hexfile=/mnt/.boot/AT841WDOG.hex
+	else
+		hexfile=$1
 	fi
+	echo "calling tiny safe boot: tsb -s /dev/ttyS3 -b $baud flash $hexfile"
+	tsb -s /dev/ttyS3 -b $baud flash $hexfile
+	if [[ $? -eq 0  ]]; then
+		if [[ $1 = "minicom" || $2 = "minicom" ]]; then
+			minicom
+		fi
+	else
+		exit 1
+	fi
+	exit 0
+}
+
+if [[ $1 = "" ]]; then
+	rebootTiny
+	programTiny
+elif [[ $1 = "-b" ]]; then
+	baud=$2
+	rebootTiny
+	programTiny $3
 else
-	exit 1
+	rebootTiny
+	programTiny $1
 fi
-exit 0
+
+	#statements

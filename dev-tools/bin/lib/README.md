7cd5079b50a6b444e4ff86c13b11ad54c75a2263
x85446 <trav@izuma.net>
2018-02-17 01:49:36 -0600

    moved the init scripts around

--- dev-tools/bin/lib/README.md
+++
@@ -1,2 +0,0 @@
-just adding a readme
-http://stackoverflow.com/questions/18770545/why-is-my-git-submodule-head-detached-from-master
--- dev-tools/bin/lib/wdogreporter.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-/home/root/devmem2 0x01c20c94 w > /wigwag/log/wdog.log
-while true; do
-/home/root/devmem2 0x01c20c94 w >> /wigwag/log/wdog.log
-done

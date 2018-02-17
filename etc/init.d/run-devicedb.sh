af1530ccd869f05adfa20ccdcc0ff9acf92f1e64
Travis McCollum <x85446@yahoo.com>
2018-02-17 02:00:15 -0600

    created the wwfunctions for sourcing wwigwag init scripts

--- etc/init.d/run-devicedb.sh
+++ etc/init.d/run-devicedb.sh
@@ -6,10 +6,10 @@ DEVICEDB_LOG="${WIGWAGROOT}/log/devicedb.log"
 DEVICEDB_CONF="${WIGWAGROOT}/etc/devicejs/devicedb.yaml"
 
 function run_devicedb() {
-    while true; do
-        $DEVICEDB_CMD start -conf=$DEVICEDB_CONF >> $DEVICEDB_LOG 2>&1
-        sleep 1
-    done
+	while true; do
+		$DEVICEDB_CMD start -conf=$DEVICEDB_CONF >> $DEVICEDB_LOG 2>&1
+		sleep 1
+	done
 }
 
 run_devicedb

c4e03d6da0bb1f33dc96813dc541ffe33e69a5e4
x85446 <trav@izuma.net>
2018-02-17 01:50:18 -0600

    fixed the devtool that inspects initramfs

--- dev-tools/scripts/extract_initramfs.sh
+++ dev-tools/scripts/extract_initramfs.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
+if [[ $1 = "--help" || $1 = "-h" || $1 = "" || $1 != *"initramfs.img" ]]; then
+	echo "Useage: $0 <initramfs.img>"
+fi
 tempdir=$(mktemp -d)
 dd if=$1 of=$tempdir/initramfs.igz bs=64 skip=1
+cd $tempdir
 cat $tempdir/initramfs.igz | gunzip | cpio -idmv
+echo "your files are in $tempdir"
 rm -rf $tempdir/initramfs.igz

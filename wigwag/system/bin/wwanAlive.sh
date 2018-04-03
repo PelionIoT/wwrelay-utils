12b0c69d2800e5635e8dc5a9366e3a1f04c37448
x85446 <trav@izuma.net>
2018-04-02 22:53:57 -0500

    build now has a mbed runner template

--- wigwag/system/bin/wwanAlive.sh
+++ wigwag/system/bin/wwanAlive.sh
@@ -1,25 +1,47 @@
 #!/bin/bash
 
 start_network(){
+	echo "starting qmi"
 	qmi-network /dev/cdc-wdm0 start
-	dhclient -v $(qmicli -d /dev/cdc-wdm0 -w)
-	echo nameserver 8.8.8.8 > /etc/resolv.conf
+	if [[ $? -eq 0 ]]; then
+		echo "start success, running dhclinet"
+		dhclient -v $(qmicli -d /dev/cdc-wdm0 -w)
+		echo nameserver 8.8.8.8 > /etc/resolv.conf
+	else
+		echo "start failed with non zero, stopping"
+		stop_network;
+	fi
 }
 
 stop_network(){
+	echo "stopping network"
 	qmi-network /dev/cdc-wdm0 stop
 	killall -9 dhclient
-	sleep 2
+	sleep 10
 
 }
-while true; do
-	sleep 3
-	status=$(qmicli -d /dev/cdc-wdm0 --wds-get-packet-service-status --device-open-proxy | awk -F ' ' '{print $4}')
-	if [[ "$status" = "'disconnected'" ]]; then
-		echo "starting network"
-		stop_network;
-		start_network;
-	else
-		echo "network is ok"
-	fi
-done
+
+loop(){
+	while true; do
+		sleep 15
+		status=$(qmicli -d /dev/cdc-wdm0 --wds-get-packet-service-status --device-open-proxy | awk -F ' ' '{print $4}')
+		if [[ "$status" = "'connected'" ]]; then
+			echo "network is ok"
+		else
+			if [[ -e /dev/cdc-wdm0 ]]; then
+				echo "disconnected: starting network"
+				stop_network;
+				start_network;
+			else
+				"/dev/cdc-wdm0 does not exist"
+			fi
+		fi
+	done
+}
+
+
+if [[ $1 = "" ]]; then
+	loop
+else
+	$1
+fi

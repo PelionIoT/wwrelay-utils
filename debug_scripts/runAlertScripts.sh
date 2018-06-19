c99ed82b412e02b7237c82fd59523d2c3bd810f5
Yash Goyal <ygoyal@wigwag.com>
2018-06-19 17:32:53 -0500

    Fixed script to run from the pwd

--- debug_scripts/runAlertScripts.sh
+++ debug_scripts/runAlertScripts.sh
@@ -2,7 +2,7 @@
 kill `ps -ef | grep alert- | awk '{print $2}'`
 sleep 2
 echo 'Starting alert-tooDark'
-/wigwag/devicejs-ng/bin/devicejs run ~/alert-tooDark.js --config=/wigwag/etc/devicejs/devicejs.conf >& tooDark.log &
+/wigwag/devicejs-ng/bin/devicejs run ./alert-tooDark.js --config=/wigwag/etc/devicejs/devicejs.conf >& tooDark.log &
 sleep 1
 echo 'Starting alert-temperatureTooLow'
-/wigwag/devicejs-ng/bin/devicejs run ~/alert-temperatureTooLow.js --config=/wigwag/etc/devicejs/devicejs.conf >& temperatureTooLow.log &
+/wigwag/devicejs-ng/bin/devicejs run ./alert-temperatureTooLow.js --config=/wigwag/etc/devicejs/devicejs.conf >& temperatureTooLow.log &

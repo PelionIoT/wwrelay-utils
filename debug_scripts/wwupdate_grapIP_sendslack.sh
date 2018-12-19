8cb26c24d5bef727b8c1f56b33ae26da050011a4
Yash Goyal <ygoyal@wigwag.com>
2018-12-19 12:31:23 -0600

    Updated the script to acquire IP when not found

--- debug_scripts/wwupdate_grapIP_sendslack.sh
+++ debug_scripts/wwupdate_grapIP_sendslack.sh
@@ -4,7 +4,11 @@
 # /etc/init.d/deviceOS-watchdog humanhalt
 
 # killall node
-ledcontrol=/wigwag/system/bin/led
+if [ ! -f ./led.sh ]; then
+    ledcontrol=/wigwag/system/bin/led
+else
+    ledcontrol=./led.sh
+fi
 source "/wigwag/system/lib/bash/relaystatics.sh"
 function grabip() {
     ifconfig > ifconfig.txt
@@ -15,21 +19,26 @@ function grabip2() {
 function dhcpthis(){
     udhcpc -n
 }
-$ledcontrol 5 5 5
-ifconfig eth0 down
-hexchars="0123456789ABCDEF"
-end=$( for i in {1..6} ; do echo -n ${hexchars:$(( $RANDOM % 16 )):1} ; done | sed -e 's/\(..\)/:\1/g' )
-ifconfig eth0 hw ether 00:a5:09$end
-ifconfig eth0 up
+$ledcontrol 255 255 255
+IP=$(ifconfig | grep -A 2 -E 'wlan|eth|wlp|enp' | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1')
+echo "Found IP=$IP"
+if [[ $IP == '' ]]; then
+    echo "Acquiring new IP..."
+    ifconfig eth0 down
+    hexchars="0123456789ABCDEF"
+    end=$( for i in {1..6} ; do echo -n ${hexchars:$(( $RANDOM % 16 )):1} ; done | sed -e 's/\(..\)/:\1/g' )
+    ifconfig eth0 hw ether 00:a5:09$end
+    ifconfig eth0 up
+    udhcpc eth0
+fi
 
-udhcpc eth0
 # /etc/init.d/devjssupport start
 
 #--------Commands go here
 useradd -p $(openssl passwd -1 maestro) maestro
 
 grabip
-dhcpthis
+# dhcpthis
 grabip2
 # sleep 15
 # curl http://localhost:3000/start
@@ -37,5 +46,5 @@ IP=$(ifconfig | grep -A 2 -E 'wlan|eth|wlp|enp' | grep -Eo 'inet (addr:)?([0-9]*
 curl -X POST -H 'Content-type: application/json' --data '{"text":"USB Reports- IP='$IP', RELAYID='$relayID'"}' https://hooks.slack.com/services/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
 # /etc/init.d/relayterm start
 # rm -rf /userdata/etc/devicejs/db/
-sleep 5
-$ledcontrol 0 10 0
+# sleep 5
+$ledcontrol 255 255 255

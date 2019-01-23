010e02b02d06bf7a93ca254d5943768ab13a86e2
Ayush Gupta <gupta11ay@gmail.com>
2019-01-23 17:12:48 +0530

    Changed the path file while requiring fetch.js

--- debug_scripts/tools/findedge-gw-dispatcher.js
+++ debug_scripts/tools/findedge-gw-dispatcher.js
@@ -3,7 +3,7 @@ var bonjour = require('bonjour')()
 const fs = require('fs');
 const chalk = require('chalk');
 const shell = require('shelljs')
-var fetchingprogram=require('/wigwag/wwrelay-utils/debug_scripts/tools/fetch.js');
+var fetchingprogram = require('./fetch.js');
 fetchingprogram.fetching(process.argv[2]).then(function(result){
     console.log(result);
 }, function(err){

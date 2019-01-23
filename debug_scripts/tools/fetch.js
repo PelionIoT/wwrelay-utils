4486d7dad7d2acd3635882fa7a0dd4eba2eb81c0
Yash Goyal <ygoyal@wigwag.com>
2019-01-23 02:58:58 -0600

    Updated the scripts to automatically fetch the eeprom from gateway dispatcher and write the eeprom

--- debug_scripts/tools/fetch.js
+++ debug_scripts/tools/fetch.js
@@ -24,13 +24,13 @@ var fetching = function(file) {
         if (!process.argv[2]) {
             console.log("Please enter the configuration file");
             reject("Please enter the configuration file")
-            //process.exit(1);
+            process.exit(1);
         } else {
             var file = process.argv[2]
             if (!fs.existsSync(file)) {
                 console.log(chalk.bold("Configuration File- " + file + " does not exist!"));
                 reject("Configuration File- " + file + " does not exist!")
-                //process.exit(1);
+                process.exit(1);
             } else {
                 if (file.split('.').pop() === 'json') {
                     findip().then(function(result) {
@@ -38,18 +38,18 @@ var fetching = function(file) {
                             if (err) {
                                 console.log(chalk.bold("Error occured while executing the shell script: " + err))
                                 reject("Error occured while executing the shell script: " + err)
-                                //process.exit(1);
+                                process.exit(1);
                             } else {
                                 console.log(chalk.bold("Shell script ran successfully "));
                                 resolve("Shell script ran successfully")
-                                //process.exit(0);
+                                process.exit(0);
                             }
                         });
                     })
                 } else {
                     console.error(chalk.bold("Please enter the file having json extension"));
                     reject("Please enter the file having json extension");
-                    //process.exit(1);
+                    process.exit(1);
                 }
 
             }
--- debug_scripts/tools/fetcheeprom.sh
+++ debug_scripts/tools/fetcheeprom.sh
@@ -5,6 +5,29 @@ bold=$(tput bold)
 fname=$(echo $filename | cut -d'.' -f 1)
 #echo $fname
 #echo $filename
+
+burnEeprom() {
+    cd /wigwag/wwrelay-utils/I2C
+    node writeEEPROM.js $file
+    if [[ $? != 0 ]]; then
+        echo "Failed to write eeprom. Trying again in 5 seconds..."
+        sleep 5
+        burnEeprom
+    fi
+}
+
+factoryReset() {
+    cd /wigwag/wwrelay-utils/debug_scripts
+    chmod 755 factory_wipe_gateway.sh
+    ./factory_wipe_gateway.sh
+}
+
+cleanLastBurn() {
+    kill $(ps aux | grep 'factory-configurator' | awk '{print $2}');
+    rm -rf mcc_config*; rm -rf pal;
+    rm -rf gateway_eeprom.json
+}
+
 if [ -e $1 ]
 then
     if [[ $filename == *.json ]]; then
@@ -19,56 +42,62 @@ then
         echo "Debuggging Values: " $radioConfig $category $cloudAddress $hardwareVersion 
         cloud=$(echo $cloudAddress|cut -c9-125)
         provisioning=$(echo $cloudAddress | cut -d'.' -f 2)
-        relayip=ifconfig eth0 2>/dev/null|awk '/inet addr:/ {print $2}'|sed 's/addr://'
+        relayip=$(ifconfig | grep -A 2 -E 'wlan|eth|wlp|enp' | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1')
         if [[ -z $radioConfig || -z $category || -z $cloudAddress || -z $hardwareVersion ]]; then
            echo "Please enter a correct configuration file" 
         else
             if [[ $ip == '' ]];then
-                echo "Please enter the IP where relay dispatcher is running"
+                echo "Please enter the IP where gateway dispatcher is running"
                 read ip
                 echo "Entered IP ${ip}"
             fi
             if [[ $ip =~ ^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
-                kill $(ps aux | grep 'factory-configurator' | awk '{print $2}');
-                rm -rf mcc_config*; rm -rf pal;
-                rm -rf eeprom.json;
                 cd /wigwag/wwrelay-utils/I2C
+                cleanLastBurn
+                echo "Starting FCC...";
+                FAIL=0
                 ./factory-configurator-client-armcompiled.elf &
-                if [ $? -eq 0 ]
-                then
-                    relayip=ifconfig eth0 2>/dev/null|awk '/inet addr:/ {print $2}'|sed 's/addr://'
-                    echo $relayip
-                    # echo $ip':5151/relay/'$provisioning'/'$cloud'/'$category'/'$hardwareVersion'/'$radioConfig'?limit=1&'$relayip
-                    curl --header "secret: WZpMyRDntxFgBGBfWPleIHzoc0egcPSsBAa8jUQw5tOgbbjc3o" 'http://'$ip':5151/relay/'$provisioning'/'$cloud'/'$category'/'$hardwareVersion'/'$radioConfig'?limit=1&'$relayip > eeprom.json
-                    file="eeprom.json"
+                echo "My IP is- $relayip"
+                # echo $ip':5151/relay/'$provisioning'/'$cloud'/'$category'/'$hardwareVersion'/'$radioConfig'?limit=1&'$relayip
+                echo "Exectuing curl command"
+                api='http://'$ip':5151/relay/'$provisioning'/'$cloud'/'$category'/'$hardwareVersion'/'$radioConfig'?limit=1&ip='$relayip''
+                curl -s --header "secret: WZpMyRDntxFgBGBfWPleIHzoc0egcPSsBAa8jUQw5tOgbbjc3o" $api > gateway_eeprom.json &
+                for job in `jobs -p`
+                do
+                echo $job
+                    wait $job || let "FAIL+=1"
+                done
+                if [ "$FAIL" == "0" ]; then
+                    echo "Done"
+                    file="gateway_eeprom.json"
                     echo "Command ran successfully"
-                    cat eeprom.json
-                    line=$(head -n 1 eeprom.json)
-                    if [[ $line = "No match Found in the database" ]]
-                    then
-                        rm -rf eeprom.json
+                    cat gateway_eeprom.json
+                    line=$(head -n 1 gateway_eeprom.json)
+                    if [[ $line = "No match Found in the database" ]]; then
+                        rm -rf gateway_eeprom.json
                     fi
-                    if [ -f "$file" ]
-                    then
-                        /etc/init.d/deviceOS-watchdog humanhalt
-                        killall maestro; killall node; killall devicedb
-                        ps aux | grep -e "node|device"
-                        rm -rf /userdata/etc/devicejs/db; rm -rf /userdata/etc/maestroConfig.db
-                        node writeEEPROM.js $file
+                    if [ -f "$file" ]; then
+                        burnEeprom
+                        factoryReset
                     else
-                        rm -rf eeprom.json
-                        echo "eeprom.json not found, unable to fetch a eeprom"
+                        rm -rf gateway_eeprom.json
+                        echo "gateway_eeprom.json not found, unable to fetch a eeprom"
+                        exit 1
                     fi
                 else
-                    echo "Command ./factory-configurator-client-armcompiled.elf failed"
+                    echo "Failed!"
+                    exit 1
                 fi
             else
                 echo "Please Enter a valid IP"
-            fi 
+                exit 1
+            fi
         fi
     else
-        echo "File should be in json format"   
+        echo "File should be in json format"
+        exit 1
     fi
 else
     echo "${1} This file does not exist"
+    exit 1
 fi

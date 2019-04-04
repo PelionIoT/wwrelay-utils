6564e2b9060f4aad77c0ddd5690cfdec37736a01
J. Michael Welsh <mike.welsh@arm.com>
2019-04-04 13:06:29 -0500

    Adding license headers to source files.
    Signed-off-by: J. Michael Welsh <mike.welsh@arm.com>

--- I2C/enableRTC_charging.js
+++ I2C/enableRTC_charging.js
@@ -1,3 +1,20 @@
+/*
+ * Copyright (c) 2018, Arm Limited and affiliates.
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 var i2c = require('i2c');
 var i2cbus = '/dev/i2c-0';
 //the PMU AXP that is used on the relay can charge the RTC battery.  In order to successfully do so, we need to tell the PMU to charge at a voltage of 2.97.  This is enabled by writing an 0x82 into the data address 0x35, of the AXP Chip (@ address 0x35) on the i2c-0 bus.
--- dev-tools/bin/panic.sh
+++ dev-tools/bin/panic.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 
+# Copyright (c) 2018, Arm Limited and affiliates.
+# SPDX-License-Identifier: Apache-2.0
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 #to moniter the progress of the panic loop, you can setup a mini server with netcat.  The following server example will simply catch the panic loop counter.  clearly you can do whatever you want with the data.  This is just an example of how to catch the data.
 
 #server side nc program to catch the count thrown from this program...

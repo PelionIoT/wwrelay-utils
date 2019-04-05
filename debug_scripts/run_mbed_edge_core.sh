dce4fe023beaed038b3fb5cfce49b8ddb8caf7ec
Yash Goyal <ygoyal@wigwag.com>
2019-04-05 01:53:24 -0500

    Monitor pt-example separately

--- debug_scripts/run_mbed_edge_core.sh
+++ debug_scripts/run_mbed_edge_core.sh
@@ -14,17 +14,19 @@ function run_edge_core() {
                 kill $(ps aux | grep '/wigwag/mbed/pt-example' | awk '{print $2}');
                 sleep 5
                 /wigwag/mbed/pt-example -n pt-example --endpoint-postfix=-$(cat /sys/class/net/eth0/address) >> /var/log/pt-example.log 2>&1
-                kill $(ps aux | grep '/wigwag/mbed/blept-example' | awk '{print $2}');
-                sleep 5
-                /etc/init.d/mept-ble start
+                # kill $(ps aux | grep '/wigwag/mbed/blept-example' | awk '{print $2}');
+                # sleep 5
+                # /etc/init.d/mept-ble start
+            fi
+        else
+            #edge-core is running, check if pt-example is, if not start it
+            if ! pgrep -x "pt-example" > /dev/null
+            then
+                /wigwag/mbed/pt-example -n pt-example --endpoint-postfix=-$(cat /sys/class/net/eth0/address) >> /var/log/pt-example.log 2>&1
             fi
         fi
         sleep 5
 	done
 }
 
-# Let this script handle it for MVP1.
-# If edge-core is started by init script then we cant start pt-example
-# So kill edge-core and let this script start it in order
-killall edge-core
 run_edge_core
